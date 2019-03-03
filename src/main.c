c76870f29b2a77abbdda18e7736d5db83f8855a4
UniverseSquared <universesquared4@gmail.com>
2019-03-03 14:49:58 +0000

    Fix indentation.

--- src/main.c
+++ src/main.c
@@ -7,42 +7,42 @@
 
 int main(int argc, char *argv[]) {
     if(SDL_Init(SDL_INIT_VIDEO) != 0) {
-	fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
-	return 1;
+        fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
+        return 1;
     }
 
     SDL_Window *window = SDL_CreateWindow("micro4",
-					  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-					  WINDOW_SIZE, WINDOW_SIZE, SDL_WINDOW_SHOWN);
+                                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                          WINDOW_SIZE, WINDOW_SIZE, SDL_WINDOW_SHOWN);
     if(!window) {
-	fprintf(stderr, "Failed to create window: %s\n", SDL_GetError());
-	SDL_Quit();
-	return 1;
+        fprintf(stderr, "Failed to create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     if(!renderer) {
-	fprintf(stderr, "Failed to create renderer: %s\n", SDL_GetError());
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-	return 1;
+        fprintf(stderr, "Failed to create renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
     }
 
     int running = 1;
     while(running) {
-	SDL_Event e;
-	while(SDL_PollEvent(&e)) {
-	    switch(e.type) {
-	    case SDL_QUIT:
-		running = 0;
-		break;
-	    }
-	}
+        SDL_Event e;
+        while(SDL_PollEvent(&e)) {
+            switch(e.type) {
+            case SDL_QUIT:
+                running = 0;
+                break;
+            }
+        }
 
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
 
-	SDL_RenderPresent(renderer);
+        SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyRenderer(renderer);
